92e61bd283c144eb8e41b90692e9db9087060853
Evelyn Andrade <eandradel2@miumg.edu.gt>
2026-07-31 21:18:33 -0600

    Maqueta para empezar con Entrega 3

---
+++ Renta de Video 2.0 OFICIAL (2)/Renta de Video 2.0 OFICIAL/Renta de Video 2.0/Clases/Cvideos.cs
@@ -0,0 +1,36 @@
+﻿using MySqlConnector;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Renta_de_Video_2._0.Clases
+{
+    internal class Cvideos
+    {
+        // Mostrar datos de la tabla video
+        public void mostrarVideos(DataGridView tablaVideos)
+        {
+            Cconexion objetoConexion = new Cconexion();
+
+            try
+            {
+                tablaVideos.DataSource = null;
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(
+                    "SELECT * FROM video;",
+                    objetoConexion.establecerConexion());
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                tablaVideos.DataSource = dt;
+
+                objetoConexion.cerrarConexion();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se lograron mostrar los registros.\nError: " + ex.Message);
+            }
+        }
+    }
+}
