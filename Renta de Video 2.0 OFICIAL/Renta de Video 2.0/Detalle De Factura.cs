122aec23ce18b29de7099175be3f6a249f757fcf
AndyGar01 <agarcial32@miumg.edu.gt>
2026-08-05 22:20:46 -0600

    Subiendo Lista Facturas + Detalle Factura

--- Renta de Video 2.0 OFICIAL/Renta de Video 2.0/Detalle De Factura.cs
+++ Renta de Video 2.0 OFICIAL/Renta de Video 2.0/Detalle De Factura.cs
@@ -1,95 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using Renta_de_Video_2._0.Clases;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Renta_de_Video_2._0
 {
-    public partial class Detalle_De_Factura : Form
-    {
-        public Detalle_De_Factura()
-        {
-            InitializeComponent();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Codigo_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Cliente_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Fecha_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TotalPagar_TextChanged(object sender, EventArgs e)
-        {
-            // regresa a la lista de facturas
-            try
-            {
-                new Lista_Facturas().Show();
-                this.Hide();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("No se pudo regresar a la lista de facturas: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-        }
-        private void CalcularTotal()
-        {
-            // suma los subtotales de la tabla para sacar el total a pagar
-            try
-            {
-                decimal total = 0;
-                foreach (DataGridViewRow fila in dataGridView1.Rows)
-                {
-                    if (fila.Cells[3].Value == null) continue;
-                    string subtotalTexto = fila.Cells[3].Value.ToString().Replace("Q", "");
-                    total += Convert.ToDecimal(subtotalTexto);
-                }
-                TotalPagar.Text = "Q" + total.ToString("0.00");
-            }
-            // si algun subtotal no se puede convertir a numero, cae aqui
-            catch (FormatException)
-            {
-                MessageBox.Show("Uno de los subtotales no es un valor numérico válido.", "Error de cálculo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                TotalPagar.Text = "Q0.00";
-            }
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-
-            {
-                Application.Exit();
-            }
-        }
-
+  public partial class Detalle_De_Factura : Form {
+   
+    public Detalle_De_Factura(){
+        InitializeComponent();
     }
+
+    // Inicio de código de "Andy Alfonso Garcia Lopez" con carné: "9959-23-1494" en la fecha de: "04/08/2026"
+    private void OnRegresar_Click(object sender, EventArgs e){
+        
+      try {
+        new Lista_Facturas().Show();
+        this.Hide();
+      }
+
+      catch (Exception ex) {
+           
+        MessageBox.Show("No se pudo regresar: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+        }
+    private void OnBuscar_Click(object sender, EventArgs e){
+
+      try {
+
+          if (string.IsNullOrWhiteSpace(txt_codigo.Text))
+          throw new Exception("Ingresa el código de la factura.");
+
+          string sCodigo = txt_codigo.Text.Trim().ToUpper();
+          string sNumero = sCodigo.Replace("FACT-", "");
+          int iIdFactura = int.Parse(sNumero);
+
+          CdetalleFactura objdetalle = new CdetalleFactura();
+          objdetalle.mostrarInfoFactura(txt_cliente, txt_fecha, txt_totalPagar, iIdFactura);
+          objdetalle.mostrarDetalleFactura(dgv_detalleFactura, iIdFactura);
+          MessageBox.Show("Búsqueda realizada.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+
+      catch (FormatException){
+      
+         MessageBox.Show("El ID debe ser un número.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+
+      catch (Exception ex) {
+           MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+
+    private void OnSalir_Click(object sender, EventArgs e) {
+        Application.Exit();
+    }
+        // Fin de código de "Andy Alfonso Garcia Lopez" con carné: "9959-23-1494" en la fecha de: "04/08/2026"
+   }
 }
--- Renta de Video 2.0 OFICIAL/Renta de Video 2.0/obj/Debug/net9.0-windows/Renta de Video 2.0.AssemblyInfo.cs
+++ Renta de Video 2.0 OFICIAL/Renta de Video 2.0/obj/Debug/net9.0-windows/Renta de Video 2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Renta de Video 2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933875236c5814ef8afeede354ec6750a9dddff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Renta de Video 2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Renta de Video 2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
